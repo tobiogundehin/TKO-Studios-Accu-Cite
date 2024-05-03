7d088ae9ffb7948df4f20cdd4164eb314a188164
valkyrie935 <114264299+valkyrie935@users.noreply.github.com>
2024-05-03 13:42:37 -0500

    Add files via upload

---
+++ full_stack_db_dump_05_03-2.sql
@@ -0,0 +1,135 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: full_stack
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `entries`
+--
+
+DROP TABLE IF EXISTS `entries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `entries` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(150) NOT NULL,
+  `year` varchar(150) DEFAULT NULL,
+  `month` varchar(15) DEFAULT NULL,
+  `day` varchar(2) DEFAULT NULL,
+  `publisher` varchar(150) DEFAULT NULL,
+  `format` varchar(150) DEFAULT NULL,
+  `summary` varchar(150) DEFAULT NULL,
+  `doi` varchar(255) DEFAULT NULL,
+  `authors` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entries`
+--
+
+LOCK TABLES `entries` WRITE;
+/*!40000 ALTER TABLE `entries` DISABLE KEYS */;
+INSERT INTO `entries` VALUES (22,'The Mythical Man-Month','1975','February','17','Addison-Wesley','Book','Computer Programming is hard, thus you must plan accordingly.',NULL,'[Frederik Brooks]'),(28,'CASH MONEY CITATIONS','2024','June','5','Get rich or die trying','AudioBook','CASH MONEY IN THE HAUZE YEEEEEEEE',NULL,'[Nina Lubinks]'),(29,'Tolerating Being Smol','2024','April','14','Brick Brick Brick','Podcast','Aubrey Talks about how to tolerate being small',NULL,'[Aubrey Kranz]'),(30,'490','2024','December','22','CAsh Money Citations','AudioBook','BOOOORRRRIIIIIINNNGGGGGG!!!!!!!',NULL,'[Mason Tulacz, Mikel Druby, Tuff Lucky]'),(33,'I Miss my Dog','1986','January','12','Rosasco institute','MLA','I miss my dog fr fr',NULL,'[Colin Burge, Stan Makowski]'),(35,'Clean Code','2008','March','30','Prentice Hall','Book','A Handbook of Agile Software Craftsmanship.',NULL,'[Tobi Ogundehin]'),(36,'The Chick Fil-A Sauce Made Me Do It','2012','April','27','Rosasco','Journal','OH nOOOOOOO',NULL,'[Nick Rosasco]'),(38,'Euphoria','2024','May','1','Def Jam','audio','Drake got Cooked',NULL,'[Kendrick Lamar, Aubrey Graham]');
+/*!40000 ALTER TABLE `entries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `library_temp`
+--
+
+DROP TABLE IF EXISTS `library_temp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `library_temp` (
+  `entryID` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `library_temp`
+--
+
+LOCK TABLES `library_temp` WRITE;
+/*!40000 ALTER TABLE `library_temp` DISABLE KEYS */;
+INSERT INTO `library_temp` VALUES (20),(15);
+/*!40000 ALTER TABLE `library_temp` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `email` char(20) DEFAULT NULL,
+  `name` varchar(150) NOT NULL,
+  `pass` char(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,NULL,'Nick',NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `libraryItems` text,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (7,'Iyanuoluwa','Ogundehin','tobi.ogundehin@valpo.edu','[22, 28, 30, 33]'),(8,'Tobi','Ogundehin','iyanuogundehin@gmail.com','[28]'),(9,'Mason','Tulacz','mason.tulacz@valpo.edu','[22]');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-03 13:10:16
